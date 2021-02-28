70997cf30a75540c16f0fd0d080bc68abe402849
s4nat <65476084+s4nat@users.noreply.github.com>
2021-02-28 10:48:33 +0800

    Update server.py

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ from pytesseract import Output
 import base64
 from PIL import Image
 import io
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/")
 def hello():
